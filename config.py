dbfce6940f055ad0900e4a5719d101916c2bac93
ayushbot1 <148586232+ayushbot1@users.noreply.github.com>
2023-10-21 16:16:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -93,7 +93,7 @@ TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 
 TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 
-STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
+STREAM_IMG_URL = "https://telegra.ph/file/d4d58c09f083b35b45956.jpg"
 
 SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
 
